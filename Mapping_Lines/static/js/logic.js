19cacef78dbdbfd34b459c579c4d40630523eefa
Praveen Kumar <mapkumar25@gmail.com>
2022-02-25 22:05:12 -0500

    Adding files

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,7 +2,9 @@
 // console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 // Create the map object with a center and zoom level.
 //let map = L.map("mapid", {
   //center: [
@@ -10,6 +12,24 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
   //],
  // zoom: 4
 //});
+// let line = [
+  // [33.9416, -118.4085],
+  // [37.6213, -122.3790]
+// ];
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [30.2672, -97.7431],
+  [43.6777, -79.6248],
+  [40.7128, -74.0060]
+];
+// Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+  // color: "red"
+// }).addTo(map);
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
 //  Add a marker to the map for Los Angeles, California.
 let marker = L.marker([34.0522, -118.2437]).addTo(map);
 // Loop through the cities array and create one marker for each city.
@@ -55,7 +75,7 @@ L.circleMarker([34.0522, -118.2437], {
   fillColor: '#ffffa1'
 }).addTo(map);
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   accessToken: API_KEY
